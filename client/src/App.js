2f9be53e3beef8d9a54c05719f8334680e692f5a
Hanami007 <bass2545b@gmail.com>
2024-10-12 12:52:04 +0700

    .

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Mapsconten from "./components/maps/Mapcontent";
+
+
+function App() {
+  return (
+    <div className="App">
+        <Mapsconten/>
+    </div>
+  );
+}
+
+export default App;
